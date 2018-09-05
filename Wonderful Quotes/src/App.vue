0b681cc2d05f7a275ea55361fe62e3d71f07c8e1
Jorgeural <jorgeural88@gmail.com>
2018-09-05 13:06:07 +0200

    Quote Form

--- Wonderful Quotes/src/App.vue
+++ Wonderful Quotes/src/App.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="container">
+        <app-new-quote @quoteAdded="addQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a Quote to delete it.
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote.vue'
+
     export default {
         components: {
-            appQuoteGrid: QuoteGrid
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
         },
         data () {
             return {
@@ -16,6 +27,11 @@ import QuoteGrid from './components/QuoteGrid.vue'
                     'Just a quote'
                 ],
                 maxQuotes: 10
+            }
+        },
+        methods: {
+            addQuote (data) {
+                this.quotes.push(data);
             }
         }
     }
