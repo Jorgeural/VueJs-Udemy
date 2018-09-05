e4ff849c135b2fc48c8b06d9eab679c6bdb9a09f
Jorgeural <jorgeural88@gmail.com>
2018-09-05 12:48:00 +0200

    Quotes layout

--- Wonderful Quotes/src/App.vue
+++ Wonderful Quotes/src/App.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="container">
-    
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue'
     export default {
-        
+        components: {
+            appQuoteGrid: QuoteGrid
+        },
+        data () {
+            return {
+                quotes: [
+                    'Just a quote'
+                ],
+                maxQuotes: 10
+            }
+        }
     }
 </script>
 
