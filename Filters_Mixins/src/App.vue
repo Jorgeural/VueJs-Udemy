1e48c5b7354ff70544e718d32a8f0053590f1e64
Jorgeural <jorgeural88@gmail.com>
2018-09-06 12:10:17 +0200

    filters

--- Filters_Mixins/src/App.vue
+++ Filters_Mixins/src/App.vue
@@ -3,6 +3,9 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
+                <p> {{ text }} No filter</p>
+                <p>{{ text | toLower }} to lower locally registered</p>
+                <p>{{ text | toUpper }} to upper globally registered</p>
 
             </div>
         </div>
@@ -11,6 +14,16 @@
 
 <script>
     export default {
+        data () {
+            return {
+               text: 'Hello world!'
+            }
+        },
+        filters: {
+            'toLower'(value) {
+                return value.toLowerCase();
+            }
+        }   
     }
 </script>
 
