cd6686e134d873bbcbc4d29101503709fcc95c97
Jorgeural <jorgeural88@gmail.com>
2018-09-07 10:31:21 +0200

    router links

--- Routing/src/components/user/User.vue
+++ Routing/src/components/user/User.vue
@@ -1,4 +1,21 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <button @click="goHome" class="btn btn-primary">Go Home</button>
+    </div>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        goHome () {
+            this.$router.push({path: '/'})
+        }
+    }
+}
+</script>
