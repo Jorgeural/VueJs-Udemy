97bce3ef6e51616bd96bb9fe81c2444430da237f
Jorgeural <jorgeural88@gmail.com>
2018-09-11 11:24:09 +0200

    axios

--- Axios/src/components/dashboard/dashboard.vue
+++ Axios/src/components/dashboard/dashboard.vue
@@ -2,8 +2,37 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>You mail address : {{ email }}</p>
   </div>
 </template>
+<script>
+  import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      email: ''
+    }
+  },
+  created() {
+    axios.get('/users.json')
+    .then((response) => {
+      console.log(response)
+      const data = response.data;
+      const users = []
+      for (let key in data) {
+          const user = data[key]
+          user.id = key
+          users.push(user)
+      }
+      this.email = users[0].email
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+}
+</script>
 
 <style scoped>
   h1, p {
